80716423ab38395c923f2c0d0480225278e9c1dc
Jacob Gehrett <jacobgehrett@gmail.com>
2021-01-26 15:48:59 -0700

    50% of Trie working

--- src/spell/Trie.java
+++ src/spell/Trie.java
@@ -1,20 +1,47 @@
 package spell;
 
+import java.util.Locale;
+
 public class Trie implements ITrie {
-    private final Node root = new Node();
-    private Node wordNode = root;
-    private static int wordCount;
-    private static int nodeCount;
+    private final INode root = new Node();
+    private int wordCount = 0;
+    private int nodeCount = 1;
 
     @Override
     public void add(String word) {
-        word.toLowerCase();
-
+        INode wordNode = root;
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); ++i) {
+            int index = (word.charAt(i) - 'a');
+            if (wordNode.getChildren()[index] == null) {
+                wordNode.getChildren()[index] = new Node();
+                ++nodeCount;
+            }
+            wordNode = wordNode.getChildren()[index];
+        }
+        wordNode.incrementValue();
+        if (wordNode.getValue() == 1) {
+            ++wordCount;
+        }
     }
 
     @Override
     public INode find(String word) {
-        return wordNode;
+        int index = 0;
+
+        INode wordNode = root;
+        word = word.toLowerCase();
+        for (int i = 0; i < word.length(); ++i) {
+            index = (word.charAt(i) - 'a');
+            if (wordNode.getChildren()[index] == null) {
+                return null;
+            }
+            wordNode = wordNode.getChildren()[index];
+        }
+        if (wordNode.getValue() > 0) {
+            return wordNode;
+        }
+        return null;
     }
 
     @Override
